c9de20ccc1ce4f1c105fc16dc26717097cf9e5fe
Maddijagadish <155830771+Maddijagadish@users.noreply.github.com>
2025-06-23 20:38:01 +0530

    Add files via upload

---
+++ 23-06-2025/sample.js
@@ -0,0 +1,76 @@
+// let output=""
+// for (let i=1;i<=10;i++){
+//     output+= i + " "
+// }
+// console.log(output.trim())
+
+// var num=1;
+// while(num<20){
+// if(num%2==0){
+// console.log(num);
+// }
+// num+=1
+// }
+
+// let c=0
+// do{
+//     console.log("Welcome..!");
+//     c++;
+// }
+// while(c<5)
+
+// for (let i = 1; i <= 3; i++) {
+//   let output = "";
+//   for (let j = 1; j <= 2; j++) {
+//     output += i + " ";
+//   }
+//   console.log(output);
+// }
+
+// arr=[1,2,3,4,5,6]
+// for (let ele of arr){
+//     console.log(ele);
+// }
+
+// var num;
+// num = 30;
+
+// if (num % 3 === 0 && num % 5 === 0) {
+//   console.log(num + " is divisible by both 3 and 5");
+// } else {
+//   console.log(num + " is NOT divisible by both 3 and 5");
+// }
+
+// let number=20;
+// let res = (number%2==0 ? "number is Even" : "number is odd");
+// console.log(res);
+
+// for (let i = 1; i <= 20; i++) {
+//   if (i % 4 === 0) {
+//     console.log(i);
+//   }
+// }
+
+
+// let age=22;
+// if(age>=18){
+//     console.log("Eligible to vote");
+// }else{
+//     console.log("Not eligible to vote");
+// }
+
+// var marks=86;
+// if(marks>90){
+//     console.log("A grade");
+// }
+// else if(marks>=75){
+//     console.log("B grade");
+// }
+// else if(marks>=50 ){
+//     console.log("C grade");
+// }
+// else{
+//     console.log("F grade");
+// }
+
+
